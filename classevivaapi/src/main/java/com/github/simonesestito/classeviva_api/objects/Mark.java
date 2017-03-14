ef8854d9bedffd6637bc8849bf39ed1fa141cbb1
Simone Sestito <simone.dev.help@gmail.com>
2017-03-14 20:20:56 +0100

    Marks: fixed wrong year

--- classevivaapi/src/main/java/com/github/simonesestito/classeviva_api/objects/Mark.java
+++ classevivaapi/src/main/java/com/github/simonesestito/classeviva_api/objects/Mark.java
@@ -1,7 +1,9 @@
 package com.github.simonesestito.classeviva_api.objects;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 
 
 public class Mark implements Serializable {
@@ -23,12 +25,15 @@ public class Mark implements Serializable {
          * First and second chars represents the day of month
          * The last two chars represents the month
          */
+        List<Integer> schoolMonths = Arrays.asList(8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7);
         Calendar result = Calendar.getInstance();
         result.set(Calendar.DAY_OF_MONTH, Integer.valueOf(date.substring(0, 2)));
-        int month = Integer.valueOf(date.substring(date.length()-2, date.length()));
+        int month = Integer.valueOf(date.substring(date.length()-2, date.length()))-1;
         result.set(Calendar.MONTH, month);
-        if (month < Calendar.getInstance().get(Calendar.MONTH))
-            result.set(Calendar.YEAR, result.get(Calendar.YEAR)-1);
+        if (schoolMonths.indexOf(month) < schoolMonths.indexOf(Calendar.getInstance().get(Calendar.MONTH))
+                && month > Calendar.getInstance().get(Calendar.MONTH)) {
+            result.set(Calendar.YEAR, Calendar.getInstance().get(Calendar.YEAR)-1);
+        }
         this.date = result;
     }
 
